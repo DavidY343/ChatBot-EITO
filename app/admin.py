08c7a66945b245e9e8b6b50ed2b3c9cba025234f
DavidY <davidym343@gmail.com>
2025-09-29 20:02:12 +0200

    Added chatmessages logs to admin dashboard

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import SimulatedUser, ChatMessage
 
+@admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ("session_id", "role", "content", "created_at")
     list_filter = ("role", "created_at")
